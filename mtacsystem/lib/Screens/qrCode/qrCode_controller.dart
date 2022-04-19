4f45e5398bb7cdadb7654886300ada57a5aa8509
Dinh Khoi <khoidv@dn.paracelsoft.com>
2022-04-19 16:50:04 +0700

    update new function QR code

--- mtacsystem/lib/Screens/qrCode/qrCode_controller.dart
+++ mtacsystem/lib/Screens/qrCode/qrCode_controller.dart
@@ -2,10 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
 import 'package:get/get.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class QRCodeController extends GetxController {
   String scanQrCode = '';
 
+  void _launchURL() async => await canLaunch(scanQrCode)
+      ? await launch(scanQrCode)
+      : throw 'Could not launch $scanQrCode';
   Future<void> scanQR() async {
     try {
       scanQrCode = await FlutterBarcodeScanner.scanBarcode(
@@ -14,13 +18,14 @@ class QRCodeController extends GetxController {
         true,
         ScanMode.QR,
       );
-      Get.snackbar(
-        "Result",
-        "QR code" + scanQrCode,
-        snackPosition: SnackPosition.BOTTOM,
-        backgroundColor: Colors.green,
-        colorText: Colors.white,
-      );
+      // Get.snackbar(
+      //   "Result",
+      //   "QR code" + scanQrCode,
+      //   snackPosition: SnackPosition.BOTTOM,
+      //   backgroundColor: Colors.green,
+      //   colorText: Colors.white,
+      // );
+      _launchURL();
     } on PlatformException {}
   }
 }
